c5cf5bbe1097501e893692624354ac4f3505a841
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-14 21:53:40 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'https://todo-app-api-two.vercel.app'
-    }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://todo-app-api-two.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
-
   plugins: [react()],
-})
+});

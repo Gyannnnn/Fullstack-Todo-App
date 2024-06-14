2e3fa6fe2846d505b7f02ffa82dd00ba21ea7e7f
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-14 21:48:17 +0530

    Update vite.config.js
    added proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':'https://todo-app-api-two.vercel.app'
+    }
+  },
+
   plugins: [react()],
 })
